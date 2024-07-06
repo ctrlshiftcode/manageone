17a86e3461124585dc6ed71c2204907189c49700
alberto.uehara@gmail.com <alberto.uehara@gmail.com>
2024-07-06 16:23:16 -0300

    Feature: First project commit

---
+++ src/main/java/io/github/manageone/ManageoneApplication.java
@@ -0,0 +1,13 @@
+package io.github.manageone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageoneApplication.class, args);
+	}
+
+}
